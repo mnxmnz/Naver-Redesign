111c893bb618143062b27796a6dee02a8b86939a
nahee-park <jessica396@ewhain.net>
2021-05-20 19:18:39 +0900

    feat: detail/Footer 전체틀 구성, 첫번째 줄 만들기, 아이콘 삽입

--- src/components/detail/footer/DetailCard.js
+++ src/components/detail/footer/DetailCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PlayIcon from '../../../assets/icons/PlayIcon.svg';
 
 //하나의 카드를 형성하는 컴포넌트, 이 컴포넌트를 map으로 받아서 footer에서 grid로 나타낼 예정임.
 function DetailCard() {
--- src/components/detail/footer/Footer.js
+++ src/components/detail/footer/Footer.js
@@ -1,23 +1,89 @@
 import React from 'react';
 import styled from 'styled-components';
 import DetailCard from '../footer/DetailCard';
+import NaverIcon from '../../../assets/icons/NaverIcon.svg';
+import PlusBtn from '../../../assets/icons/PlusBtn.svg';
 
 function Footer() {
   return (
     <FooterWrap>
-      <DetailCard />
-      <div>Detail - Footer</div>
+      <div className="introduce">
+        <div className="introduce--title">스포츠 영상</div>
+        <div className="introduce--detail">주제별로 분류된 다양한 영상 모음</div>
+      </div>
+      <div className="DetailCard-container">
+        {/* 4*5 grid 틀에 map 돌려서 DetailCard넣을 예정 */}
+        <DetailCard />
+      </div>
+      {/* margin-top에 -값 주거나 해서 잘 올려보기*/}
+      <div className="more">
+        <img src={PlusBtn} className="more--button" />
+        <div className="mare--text">새로운 글 더보기</div>
+      </div>
+      <div className="footer">
+        <div className="footer--second">
+          본 콘텐츠의 저작권은 네이버 및 제공처에 있으며, 이를 이용하는 경우 저작권법 등에 따라
+          법적책임을 질 수 있습니다.
+        </div>
+        <div className="footer--last">
+          {/* 배열 map으로 돌면서 그리도록 .. 각각에 제어권 부여하기 위해서  */}
+          <img src={NaverIcon} className="footer--last--navericon" />
+          <div className="footer--last--">Copyright ⓒ NAVER Corp. All Rights Reserved.</div>
+        </div>
+      </div>
     </FooterWrap>
   );
 }
 
 const FooterWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   padding-left: ${({ theme }) => theme.pcPadding.detail};
   padding-right: ${({ theme }) => theme.pcPadding.detail};
 
   @media (max-width: 1200px) {
     padding-left: ${({ theme }) => theme.tabletPadding.detailFooter};
     padding-right: ${({ theme }) => theme.tabletPadding.detailFooter};
+  }
+  .introduce {
+    width: 120rem;
+    height: 2.3rem;
+    display: flex;
+    align-items: center;
+    &--title {
+      width: 8.3rem;
+      height: 2.3rem;
+      font-style: normal;
+      font-weight: bold;
+      font-size: 1.9rem;
+      line-height: 2.3rem;
+      /* identical to box height */
+
+      letter-spacing: -0.05em;
+
+      /* text_black */
+
+      color: #202020;
+    }
+    &--detail {
+      width: 19.5rem;
+      height: 1.8rem;
+      font-family: Apple SD Gothic Neo;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 1.5rem;
+      line-height: 1.8rem;
+      text-align: center;
+      /* identical to box height */
+
+      letter-spacing: -0.02rem;
+
+      /* text_black */
+
+      color: #202020;
+      margin-left: 92.3rem;
+    }
   }
 `;
 
